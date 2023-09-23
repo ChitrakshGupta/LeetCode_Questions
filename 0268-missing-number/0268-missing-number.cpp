a611261ab666127c7f72b8b0539c112ae42e68ad
Chitraksh Gupta <95366435+ChitrakshGupta@users.noreply.github.com>
2023-09-23 22:47:26 +0530

    Time: 20 ms (23.34%), Space: 18.4 MB (8.73%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,13 +3,29 @@ public:
     int missingNumber(vector<int>& nums) {
         // apparoch 1 -sum
         int n=size(nums);
-        int des_sum=n*(n+1)/2;
-        int sum=0;
-        for(int i: nums){
-            sum+=i;
+        
+        
+        // int des_sum=n*(n+1)/2;
+        // int sum=0;
+        // for(int i: nums){
+        //     sum+=i;
+        // }
+        // des_sum=des_sum-sum;
+        // return des_sum;
+        
+        
+        // aparoch 2 xor gate 
+        // for all values of array
+        int xorr=0;
+        for(int i=0; i<n; i++){
+            xorr ^=nums[i];
         }
-        des_sum=des_sum-sum;
-        return des_sum;
+        // for all number 0,1,2,3,4,---n
+         for(int i=0; i<=n; i++){
+            xorr ^=i;
+        }
+        
+        return xorr;
         
         
     }
