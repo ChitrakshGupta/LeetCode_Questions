ca437e5955864252c69e546df3b18711167d8059
Chitraksh Gupta <95366435+ChitrakshGupta@users.noreply.github.com>
2024-01-08 21:54:32 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (10.72%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,11 +1,36 @@
 class Solution {
 public:
-    int fib(int n) {
-        //base case resursice solution
+    int solveRecursion(int n) {
+        //base case resursive solution
         if(n==0 || n==1){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+        return solveRecursion(n-1)+solveRecursion(n-2);
+        
+        
+    }
+    int solveDp(int n, vector <int> &dp ){
+                if(n==0 || n==1){
+            return n;
+        }
+        // step 3: if answer alerdy exist then return the answer from array
+        // agr pahele se calculate kr rkha ho to 
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        
+        //STEP2: store and return the dp array;
+        dp[n]=solveDp(n-1,dp)+solveDp(n-2,dp);
+        return dp[n];
+        
+    }
+    int fib(int n) {
+        //STEP 1: crate dp array 
+     
+        vector <int> dp(n+1,-1);
+        //STEP1 *: pass it in a function 
+        int ans=solveDp(n,dp);
+        return ans;
         
         
     }
